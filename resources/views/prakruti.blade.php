8ed92e3151cee5a8665c5c155b502db66279fa0a
bhoomiT7 <135812959+bhoomiT7@users.noreply.github.com>
2024-02-12 15:35:14 +0530

    changes

---
+++ resources/views/prakruti.blade.php
@@ -0,0 +1,7 @@
+<div class="container">
+<div class="main-text">
+    <h1>Body type-Prakruti Analysis Test</h1>
+    <h5>which highly appky to you</h5>
+</div>
+
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AnalysisController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('prakruti');
 });
+//Route::get('/prakruti',[AnalysisController::class,'index']);
